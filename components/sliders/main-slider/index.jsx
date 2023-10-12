208a49de113a58eb7f45e579b77374afae78fa17
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-12 20:34:53 +0330

    ssliders completed

--- components/sliders/main-slider/index.jsx
+++ components/sliders/main-slider/index.jsx
@@ -1,3 +1,5 @@
+import SliderComp from "./slider-comp";
+import MainSliderAds from "./ads";
 import HotNews from "./hot-news";
 
 const MainSlider = () => {
@@ -5,10 +7,11 @@ const MainSlider = () => {
       <section className=" bg-zinc-100 rounded-lg p-4 flex flex-col gap-6 ">
          <HotNews/>
          <div className=" flex justify-between items-center gap-4 ">
-         Test
+            <SliderComp/>
+            <MainSliderAds/>
          </div>
       </section>
-   ); 
-}
+   );
+} 
 
 export default MainSlider;
