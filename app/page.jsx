75051eca25f074529777e80793b6a2c550351365
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-13 21:05:26 +0330

    posts section started

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import MainPageCategories from "@/components/main-page-categories";
+import MainPagePopularPosts from "@/components/main-page-popular-posts";
 import MainSlider from "@/components/sliders/main-slider";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
    <main className=" flex flex-col gap-12 " >
     <MainSlider />
     <MainPageCategories />
+    <MainPagePopularPosts />
    </main>
   )
 }
---
+++ components/main-page-popular-posts/index.jsx
@@ -0,0 +1,13 @@
+
+const MainPagePopularPosts = () => {
+
+  
+
+   return (
+      <section className=" mx-8 p-4 bg-zinc-100 rounded-lg flex flex-col gap-6 ">
+        posts
+      </section>
+   );
+}
+
+export default MainPagePopularPosts;
