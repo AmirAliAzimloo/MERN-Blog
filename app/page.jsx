f95bed4a9a6868e33ea306bd60889d996241a2bf
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-18 16:29:55 +0330

    ads completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import MainPageCategories from "@/components/main-page-categories";
 import MainPagePopularPosts from "@/components/main-page-popular-posts";
 import MainSlider from "@/components/sliders/main-slider";
+import MainPageAdsSection from "@/components/main-page-ads-section";
 
 export default function Home() {
   return (
    <main className=" flex flex-col gap-12 " >
     <MainSlider />
     <MainPageCategories />
-    <MainPagePopularPosts />
+    <MainPagePopularPosts />         
+    <MainPageAdsSection/>
    </main>
   )
 }
