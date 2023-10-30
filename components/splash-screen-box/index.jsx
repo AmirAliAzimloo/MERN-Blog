6d48e9cb5f5bc86a215a7c18b16b1a96c102fdbe
Amir Ali <amiraliazimloo123@gmail.com>
2023-10-30 22:11:52 +0330

    splash screen completed

--- components/splash-screen-box/index.jsx
+++ components/splash-screen-box/index.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import 'animate.css';
 
 const SplashScreenBox = () => {
    return (
-    <>
-    test
-    </>
+      <div className=" flex justify-center items-center h-[100vh] ">
+         <div className=" flex flex-col gap-4 items-center">
+            <div className="  w-[5rem] h-[5rem] relative  animate__animated animate__slow animate__flip">
+               <Image src={"/logo-70.png"} fill className="object-cover" alt="splash screen" />
+            </div>
+            <p className="  animate__animated animate__slower animate__fadeInUp">Amir Ali Azimloo</p>
+         </div>
+      </div>
    );
 }
 
